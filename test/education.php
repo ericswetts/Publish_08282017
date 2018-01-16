7714fec75a910494c38d492eab25c92d9eeb455d
Fallenreaper <wllm.francis@gmail.com>
2018-01-15 20:28:01 -0500

    A quick POC

---
+++ test/education.php
@@ -0,0 +1,21 @@
+<html>
+<?php require_once("head_content.inc"); ?>
+<body class="background_image">
+<?php
+	#Education
+	require_once("header.inc");
+	require_once("menu.inc");
+
+?>
+<div class="container-fluid">
+	<div class="row row_eq_height">
+		<div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
+		Tuh-Wat
+		</div>
+	</div>
+</div>
+<?php
+	require_once("footer.inc");
+?>
+</body>
+</html>
