903c21b9ee48e5b680bba8787271e602bf4629c5
Fallenreaper <wllm.francis@gmail.com>
2018-01-15 20:31:53 -0500

    Added Educational Data.

--- test/education.php
+++ test/education.php
@@ -7,10 +7,39 @@
 	require_once("menu.inc");
 
 ?>
+<div>
+<div class="container-fluid header_spacer"> <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1 h1">
+    <div class="h">Education</div> </div> </div> </div>
+</div>
+
 <div class="container-fluid">
 	<div class="row row_eq_height">
 		<div class="col-xs-12 col-sm-12 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
-		Tuh-Wat
+		<!-- Eric, Based on what your current demo was looking like, you would want to put content related to you in some way here. -->
+		<div class="ct">
+
+<p>In 2012, I completed my B.A. in Urban Studies, with a concentration in community organization and a minor in Africana Studies, from the University of
+Pittsburgh’s Dietrich School of Arts and Sciences. My degree prepared me to join the non-profit sector to complete work related to community organizing,
+economic development, and capacity-building in economically depressed communities.  A core component of my studies was the study of race in the American
+context, the exploration of intersecting identities and how they pertain to privilege, and the legacy of oppression of black Americans. This education served as a foundation for my
+    interest in the social determinants of health, and would steer my educational interests moving forward. </p>
+
+
+
+<p>In 2017, I completed my Master of Public Health (MPH) degree from the University of Pittsburgh Graduate School of Public Health. This education built on my
+undergraduate education, helping me understand not only what health disparities exist, but providing the frameworks and skillsets necessary to address them.
+   </p>
+<div class="pop">
+<p>Today, I seek research opportunities related to child and maternal health in low and middle income countries (LMIC), particularly in East and Sub-Saharan Africa.
+    Areas of interest include:</p>
+    <ul>
+        <li>&bull; Program development  aimed at addressing cervical cancer incidence in HIV+ women and girls through HPV vaccination </li>
+        <li>&bull; Training HIV Community Health workers to provide home-based HIV testing and counseling to women</li>
+        <li>&bull; M-health initiatives to increase ART adherence</li>
+    </ul>
+        </div>
+    </div>
 		</div>
 	</div>
 </div>
